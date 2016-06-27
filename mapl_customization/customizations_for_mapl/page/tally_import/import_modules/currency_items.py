ac1bcd076b0f71a2876665ef05ea6bbd66e0544a
akshay <mehta.akshay@gmail.com>
2016-06-27 20:34:45 +0530

    Functionality Changes

---
+++ mapl_customization/customizations_for_mapl/page/tally_import/import_modules/currency_items.py
@@ -0,0 +1,21 @@
+import frappe
+
+class TallyImportCurrencyItems:
+    def __init__(self, value):
+        self.currency_table_map = { 'MAILINGNAME' : 'name',
+                    'DECIMALSYMBOL' : 'fraction',
+                    'ORIGINALNAME' : 'symbol'
+                   }
+        self.process_node = value
+        self.process()        
+
+    def process(self):
+        print "----------RESULT----------"
+        print "PROCESSED:" + self.process_node['MAILINGNAME']
+        print "----------RESULT END----------"
+        currency_doc = frappe.db.exists('Currency', self.process_node['MAILINGNAME'])
+        if currency_doc == False:
+            currency_doc = frappe.get_doc('Currency', self.process_node['MAILINGNAME'])
+            currency_doc.fraction = process_node['DECIMALSYMBOL']
+            currency_doc.symbol = process_node['ORIGINALNAME']            
+            currency_doc.insert(ignore_permissions=True)
--- mapl_customization/customizations_for_mapl/page/tally_import/tally_import.py
+++ mapl_customization/customizations_for_mapl/page/tally_import/tally_import.py
@@ -1,6 +1,7 @@
 import frappe
 import json
 import xmltodict
+from mapl_customization.customizations_for_mapl.page.tally_import.import_modules.currency_items import TallyImportCurrencyItems
 
 @frappe.whitelist()
 def read_uploaded_file(ignore_encoding=False):
@@ -11,6 +12,17 @@ def read_uploaded_file(ignore_encoding=False):
 		from frappe.utils.file_manager import get_uploaded_content
 		fname, fcontent = get_uploaded_content()	
 
-	xmlstr = xmltodict.parse(fcontent)
-	return xmlstr
+	xmlstr = xmltodict.parse(fcontent, item_depth=5, item_callback=process)
 
+def process_dict(xmlstr):
+    stock_dict = xmlstr
+    stock_messages = sd['ENVELOPE']['BODY']['IMPORTDATA']['REQUESTDATA']['TALLYMESSAGE']
+
+def process(path, item):
+	if (isinstance(item, (dict, set))):	
+		if (item.has_key('CURRENCY')):
+			TallyImportCurrencyItems(item['CURRENCY'])
+
+	return True
+
+	
