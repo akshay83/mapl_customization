5226887c26ad4328b100bb96bce652b72e08b3c3
akshay <mehta.akshay@gmail.com>
2016-07-04 20:54:26 +0530

    Was a Simple Change :-<

--- mapl_customization/public/js/form_comments.js
+++ mapl_customization/public/js/form_comments.js
@@ -13,44 +13,16 @@ $(document).ready(function() {
 
 $(document).on("page-change", function() {
         customized_footer = 0;
-        console.log("Called Page, Route:"+frappe.get_route_str());
-        current_form = frappe.get_route()[1];
-        if (frappe.get_route()[0] == 'Form' && $.inArray(current_form, visited_documents) == -1) {
-            try {
-                if ($('.form-footer').parent()[0].className=='row layout-main') {
-                    console.log("Removed Button:"+$('.pull-right.scroll-to-top').remove().length);
-                    $('.form-footer').remove();
-                }
-            } catch (err) {
-                console.log(err);
-            } finally {
-                visited_documents.push(current_form);
-            }
-        }
         if (frappe.get_route()[0] != 'Form')  {
             $('#comments-button').hide();
         } else {
             $('#comments-button').show();
         }
-        console.log('Button Length:'+$('.pull-right.scroll-to-top').length);
 });
 
-
-$('div').on('DOMNodeInserted', '.form-footer', function (e) {
-    if (e.target.className == 'form-footer' && customized_footer!=1) {
-            console.log("Trying to Insert");
-        if ($('.form-footer').parent().length == 1 && $('.form-footer').parent()[0].className != 'row layout-main') {
-            customized_footer = 1;
-            $('.form-footer').appendTo($('.row.layout-main'))
-            console.log('Current Buttons:'+$('.pull-right.scroll-to-top').length);
-            $('.pull-right.scroll-to-top').appendTo($('.row.layout-main'))
-            $('div').find('.form-footer').hide();
-        }
-    }
-});
 
 doComments = function() {
-    var comments = $('div').find('.form-footer');
+    var comments = $('div').find('.after-save');
     if (comments.is(":visible")) { 
         comments.hide();
     }
@@ -66,9 +38,9 @@ $(document).click(function(e){
     if (e.target.id == 'comments-button') {
         return;
     }
-    if( $(e.target).closest('.form-footer').length > 0 ) {
+    if( $(e.target).closest('.after-save').length > 0 ) {
         return;
     }
-    $('div').find('.form-footer').hide();
+    $('div').find('.after-save').hide();
 
 });
