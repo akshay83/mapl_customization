70bacf105a3f87f6d0be90a19c959bd016485bf2
akshay <mehta.akshay@gmail.com>
2017-05-08 19:13:51 +0530

    [Reports] DCR & Finance Outstanding

---
+++ mapl_customization/customizations_for_mapl/report/daily_collection_report/daily_collection_report.js
@@ -0,0 +1,23 @@
+frappe.query_reports['Daily Collection Report'] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label":__("From Date"),
+			"fieldtype": "Date",
+			"default": get_today()
+		},
+		{
+			"fieldname":"to_date",
+			"label":__("To Date"),
+			"fieldtype": "Date",
+			"default": get_today()
+		},
+		{
+			"fieldname":"let",
+			"label":__("Letter Head"),
+			"fieldtype": "Link",
+			"options": "Letter Head",
+			"default": "Vijay Nagar"
+		}
+	]
+}
