d07134b744d6c33ae3ea8ba56a6d0806dbae8dad
akshay <mehta.akshay@gmail.com>
2017-06-21 14:22:19 +0530

    Payment Entry Monkey Patch

--- mapl_customization/customizations_for_mapl/monkey_patch.py
+++ mapl_customization/customizations_for_mapl/monkey_patch.py
@@ -42,7 +42,7 @@ def allow_transition_from_0_to_2(self, docstatus):
 		Valid transitions are (number in brackets is `docstatus`):
 		- Save (0) > Save (0)
 		- Save (0) > Submit (1)
-        - Save (0) > Cancel (2)
+		- Save (0) > Cancel (2)
 		- Submit (1) > Submit (1)
 		- Submit (1) > Cancel (2)
 		If docstatus is > 2, it will throw exception as document is deemed queued
@@ -76,9 +76,68 @@ def allow_transition_from_0_to_2(self, docstatus):
 		elif docstatus==2:
 			raise frappe.ValidationError, _("Cannot edit cancelled document")
 
+
+def add_party_gl_entries(self, gl_entries):
+	if self.party_account:
+		if self.payment_type=="Receive":
+			against_account = self.paid_to
+		else:
+			against_account = self.paid_from
+		
+				
+		party_gl_dict = self.get_gl_dict({
+			"account": self.party_account,
+			"party_type": self.party_type,
+			"party": self.party,
+			"against": against_account,
+			"account_currency": self.party_account_currency
+		})
+		
+		# Changed the following line in Monkey Patch
+		dr_or_cr = "credit" if self.payment_type == "Receive" else "debit"
+		
+		for d in self.get("references"):
+			gle = party_gl_dict.copy()
+			gle.update({
+				"against_voucher_type": d.reference_doctype,
+				"against_voucher": d.reference_name
+			})
+			
+			allocated_amount_in_company_currency = flt(flt(d.allocated_amount) * flt(d.exchange_rate), 
+				self.precision("paid_amount"))	
+			
+			gle.update({
+				dr_or_cr + "_in_account_currency": d.allocated_amount,
+				dr_or_cr: allocated_amount_in_company_currency
+			})
+			
+			gl_entries.append(gle)
+			
+		if self.unallocated_amount:
+			base_unallocated_amount = base_unallocated_amount = self.unallocated_amount * \
+				(self.source_exchange_rate if self.payment_type=="Receive" else self.target_exchange_rate)
+				
+			gle = party_gl_dict.copy()
+			
+			gle.update({
+				dr_or_cr + "_in_account_currency": self.unallocated_amount,
+				dr_or_cr: base_unallocated_amount
+			})
+
+			gl_entries.append(gle)
+
+
+def validate_payment_against_negative_invoice(self):
+	pass
+
+def monkey_patch_payment_entry_validate():
+	from erpnext.accounts.doctype.payment_entry import payment_entry
+	payment_entry.PaymentEntry.validate_payment_against_negative_invoice = validate_payment_against_negative_invoice
+	payment_entry.PaymentEntry.add_party_gl_entries = add_party_gl_entries
+
 def monkey_patch_allow_transition_from_0_to_2():
-    from frappe.model.document import Document
-    Document.check_docstatus_transition = allow_transition_from_0_to_2
+	from frappe.model.document import Document
+	Document.check_docstatus_transition = allow_transition_from_0_to_2
 
 def monkey_patch_for_allow_on_submit():
 	from frappe.custom.doctype.customize_form.customize_form import CustomizeForm
@@ -92,8 +151,9 @@ def monkey_patch_for_serial_validation():
 
 	from monkey_patch_serial import set_purchase_details as spd
 	serial_no.SerialNo.set_purchase_details = spd 
-	
+
 def do_monkey_patch():
 	monkey_patch_for_allow_on_submit()
 	monkey_patch_allow_transition_from_0_to_2()
 	monkey_patch_for_serial_validation()
+	monkey_patch_payment_entry_validate()
