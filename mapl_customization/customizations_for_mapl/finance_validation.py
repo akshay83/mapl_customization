eeeffdd64e50f4fa859f1baea9134398184844dd
akshay <mehta.akshay@gmail.com>
2017-07-20 15:10:35 +0530

    [Vehicle] Sales Fields

--- mapl_customization/customizations_for_mapl/finance_validation.py
+++ mapl_customization/customizations_for_mapl/finance_validation.py
@@ -1,7 +1,9 @@
 import frappe
+from frappe.utils import cint
 
 def sales_invoice_validate(doc, method):
 	finance_validate(doc, method)
+	vehicle_validation(doc, method)
 
 def sales_order_validate(doc, method):
 	finance_validate(doc, method)
@@ -13,3 +15,8 @@ def finance_validate(doc, method):
 def payment_entry_validate(doc, method):
 	from frappe.utils import money_in_words
 	doc.in_words = money_in_words(doc.paid_amount);
+
+def vehicle_validation(doc, method):
+	for i in doc.items:
+		if cint(i.is_vehicle) and len(doc.items)>1:
+			frappe.throw("A Sales Invoice can have only Single Vehicle and No Other Item")
