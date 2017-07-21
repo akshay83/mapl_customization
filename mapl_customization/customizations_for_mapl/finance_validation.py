4314d47909b04710e34b28597f62fff55fd8cd05
akshay <mehta.akshay@gmail.com>
2017-07-21 20:46:51 +0530

    [minor] Changes

--- mapl_customization/customizations_for_mapl/finance_validation.py
+++ mapl_customization/customizations_for_mapl/finance_validation.py
@@ -3,6 +3,8 @@ from frappe.utils import cint
 
 def sales_invoice_validate(doc, method):
 	finance_validate(doc, method)
+	negative_stock_validation(doc, method)
+	validate_hsn_code(doc, method)
 	vehicle_validation(doc, method)
 
 def sales_order_validate(doc, method):
@@ -23,3 +25,13 @@ def vehicle_validation(doc, method):
 
 		if cint(i.is_vehicle) and len(i.serial_no.split('\n'))>1:
 			frappe.throw("A Sales Invoice can have only ONE Vehicle")
+
+def negative_stock_validation(doc, method):
+	for i in doc.items:
+		if i.actual_qty <= 0:
+			frappe.msgprint("Negative Stock for {0}, Please verify before Submitting".format(i.item_code))
+
+def validate_hsn_code(doc, method):
+	for i in doc.items:
+		if not i.gst_hsn_code:
+			frappe.throw("HSN Code not found for {0}".format(i.item_code))
