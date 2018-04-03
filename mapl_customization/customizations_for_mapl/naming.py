8c544755519aef8541d94dd463693c10ee91c10e
akshay <mehta.akshay@gmail.com>
2018-04-03 15:39:09 +0530

    [Fix] Naming Before 1 Apr 18

--- mapl_customization/customizations_for_mapl/naming.py
+++ mapl_customization/customizations_for_mapl/naming.py
@@ -8,8 +8,6 @@ def set_auto_name(doc, method):
 
 	dt_meta = frappe.get_meta(doc.doctype)
 
-	#print "DEBUG:"+("YES" if dt_meta.get_field("reporting_name") else "NO")
-
 	location = "VN"
 
 	docs = 	{
@@ -28,9 +26,7 @@ def set_auto_name(doc, method):
 			"Stock Entry", "Journal Entry", "Sales Order", "Purchase Receipt", "Quotation"):
 
 		user = frappe.session.user
-		#print "DEBUG:"+user
 		user_group = frappe.db.get_value("User", user, "user_group")
-		#print "DEBUG:"+(user_group if user_group else "")
 
 		if user_group:
 			if (user_group.lower() == "geeta bhawan"):
@@ -40,16 +36,14 @@ def set_auto_name(doc, method):
 		#print "DEBUG:"+location
 
 		abbr = frappe.db.get_value("Company", doc.company, "abbr")
-		#print "DEBUG:"+abbr
 
-		dt = doc.posting_date if doc.posting_date else today()
+		docdt = doc.posting_date if doc.posting_date else today()
 		if doc.doctype in ("Sales Order", "Purchase Receipt"):
-			dt = doc.transaction_date
-		fiscal_year =  str(get_fiscal_year(date=dt)[0])
+			docdt = doc.transaction_date
+		fiscal_year =  str(get_fiscal_year(date=docdt)[0])
 		#print "DEBUG:"+fiscal_year
 
 		short_fiscal_year = fiscal_year[2:4] + "-" + fiscal_year[7:9]
-		#print "DEBUG:"+short_fiscal_year
 
 		dt = docs.get(doc.doctype,"")
 		if doc.doctype not in ("Payment Entry", "Journal Entry", "Stock Entry", "Quotation", "Sales Order"):
@@ -60,14 +54,20 @@ def set_auto_name(doc, method):
 				dt = "PAY"
 		#print "DEBUG:"+dt
 
-		nm = make_autoname(key=abbr+"/"+dt+"/"+location+"/"+short_fiscal_year+"/"+".######", doctype=doc.doctype)
+		nm = ""
+		if getdate(docdt)<getdate("2018-04-01"):
+			if doc.doctype != "Journal Entry":
+				nm = make_autoname(key=abbr+"/"+location+"/"+dt+"/"+".YYYY."+"/"+".######", doctype=doc.doctype)
+			else:
+				nm = make_autoname(key=abbr+"/"+"JV"+"/"+".YYYY."+"/"+".######", doctype=doc.doctype)
+		else:
+			nm = make_autoname(key=abbr+"/"+dt+"/"+location+"/"+short_fiscal_year+"/"+".######", doctype=doc.doctype)
+
+			if dt_meta.get_field("reporting_name"):
+				#print "DEBUG:"+location+"/"+short_fiscal_year+"/"+nm[-6:]
+				doc.reporting_name = ("R" if "-RET" in nm else "")+location+"/"+short_fiscal_year+"/"+nm[-6:]
+
 		#print "DEBUG:"+nm
 		doc.name = nm
-
-		if dt_meta.get_field("reporting_name"):
-			#print "DEBUG:"+location+"/"+short_fiscal_year+"/"+nm[-6:]
-			doc.reporting_name = ("R" if "-RET" in nm else "")+location+"/"+short_fiscal_year+"/"+nm[-6:]
-
-		#frappe.throw("Testing")
 
 	#print "DEBUG:-------------------------------------------------------------"
