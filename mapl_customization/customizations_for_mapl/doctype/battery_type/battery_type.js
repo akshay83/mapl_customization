43fc73a3ddb7ce1f76c3911f1d748c4dd72fe00d
Akshay Mehta <mehta.akshay@gmail.com>
2022-02-22 12:48:00 +0000

    Battery Manufacturer Import Fixes

---
+++ mapl_customization/customizations_for_mapl/doctype/battery_type/battery_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Akshay Mehta and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Battery Type', {
+	// refresh: function(frm) {
+
+	// }
+});
